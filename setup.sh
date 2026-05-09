247655f7526625bcbbc64d0aead24c71a3efe394
LordFreedom <148976788+octseventen@users.noreply.github.com>
2026-05-09 21:21:08 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
 echo "Server Connected" > /etc/handeling >/dev/null 2>&1
 function CEKIP () {
 MYIP=$(curl -sS ipv4.icanhazip.com)
-IPVPS=$(curl -sS https://raw.githubusercontent.com/octseventen/VIP/main/izin-script/main/ip | grep $MYIP | awk '{print $4}')
+IPVPS=$(curl -sS https://raw.githubusercontent.com/octseventen/izin-script/main/ip | grep $MYIP | awk '{print $4}')
 if [[ $MYIP == $IPVPS ]]; then
 domain
 Pasang
